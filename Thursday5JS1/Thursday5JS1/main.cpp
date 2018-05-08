5b40dabc03c9261153511c7d9d906b2e88baa85e
Martin Heffernan <n1408364@qut.edu.au>
2018-05-08 20:03:09 +1000

    Flex tutorial completed

---
+++ Thursday5JS1/Thursday5JS1/main.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h> 
+#include <y.tab.h> 
+
+int yylex();
+
+YYSTYPE yylval;
+extern FILE *yyin;
+
+int main(int argc, char* argv[]) {
+	fopen_s(&yyin, argv[1], "r");
+
+	int token;     
+	do { 
+		token = yylex();         
+		printf("next token: ");         
+		switch (token) 
+		{ 
+			case NUMBER:                 
+				printf("NUMBER (%d)\n", yylval.num);                 
+				break;             
+			case IDENT:                 
+				printf("IDENT (%s)\n", yylval.name);                 
+				break;             
+			case IF:                 
+				printf("IF\n");                 
+				break;             
+			case ELSE:                 
+				printf("ELSE\n");                 
+				break;             
+			case INT:                 
+				printf("INT\n");                 
+				break;             
+			case BOOL:                 
+				printf("BOOL\n");                 
+				break;             
+			case 0:                 
+				printf("EOF\n");                 
+				break;             
+			default:                 
+				printf("'%c'\n", token);                 
+				break; 
+		} 
+	} 
+	while (token != 0);
+
+	return 0;
+}
